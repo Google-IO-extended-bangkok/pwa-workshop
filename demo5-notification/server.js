d69232c6bcb16a58fb7db26430f2c8a001915bd8
warat wongmaneekit <warat@zocialinc.com>
2016-06-22 22:44:35 +0700

    Fix bug for local forage

--- demo5-notification/server.js
+++ demo5-notification/server.js
@@ -6,7 +6,7 @@ var message = new gcm.Message({
 
 // Set up the sender with you API key, prepare your recipients' registration tokens.
 var sender = new gcm.Sender('AIzaSyC-IR5Ru9-g2I2iqrmhWyoT8alcKhT6FlM');
-var regTokens = ['fuebE6moZKU:APA91bEaNgzLw1uD7rjoKNX3yzwQvvnlJJbdNeHgUHdYRprcCvJ7qlXIc49cd29NCa-iVrOvj0QsMXpf11w627jepwaE35S7J_kjJNGYDEKhPo08Mc5-QTe8gZZ7hDI9qGiOB7_tkBiq'];
+var regTokens = ['cHn6KFYaccc:APA91bG-3uum1QP3rngNJoCTNt1r3jtP8jZM8eO6zsFVybhYVed-vQWBX26D5G3I26IzOhuQuZues4T4mQuRlmcvYrDBPIRnqBw6n-szfIkWB1pnEMmIIjbvGk-4DIT1sclr4lTuEzUO'];
 
 sender.send(message, { registrationTokens: regTokens }, (err, response) => {
     if(err) console.error(err);
